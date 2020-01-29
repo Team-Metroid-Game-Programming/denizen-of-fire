d6af303c2fdebb83580d93671fd1015da15b29f3
Smie <smie.grin@gmail.com>
2020-01-28 21:50:55 -0600

    Camera Bounded by Room Box.

---
+++ Assets/Scripts/Scoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoot : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dx = speed*Input.GetAxis("Horizontal");
+        var dy = speed*Input.GetAxis("Vertical");
+        var input = new Vector3(dx,dy,0);
+        transform.position = input+transform.position;
+    }
+}
