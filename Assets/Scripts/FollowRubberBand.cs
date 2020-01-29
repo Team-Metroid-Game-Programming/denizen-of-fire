f4f585a713c549c0094d6262bec08cb819f7ea2d
Smie <smie.grin@gmail.com>
2020-01-28 21:50:55 -0600

    Camera Bounded by Room Box.

---
+++ Assets/Scripts/FollowRubberBand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowRubberBand : MonoBehaviour
+{
+    [SerializeField]
+    float Strength = 1;
+    [SerializeField]
+    Transform Target;
+    Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = transform.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dir = new Vector2(Target.position.x-transform.position.x, Target.position.y-transform.position.y).normalized;
+        body.AddForce(dir*Strength);
+    }
+}
