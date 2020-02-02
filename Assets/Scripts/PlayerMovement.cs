f58eee2c10a37f38c4de485ab3a8dcdaf3e9f7b0
Jacob Holst <jacob.holst@jacks.sdstate.edu>
2020-02-02 00:01:39 -0600

    Added flaire movement and jump

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Animator animator;
+    private float horizontalMovement = 0f;
+    private bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Move our character
+        characterController.Move(horizontalMovement, false, jump);
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+        jump = false;
+    }
+}
