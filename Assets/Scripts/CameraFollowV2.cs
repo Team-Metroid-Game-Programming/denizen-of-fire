31000febd1044af6fd7d4667d88cb5dacd677313
purpledoop <60326139+purpledoop@users.noreply.github.com>
2020-02-08 13:21:26 -0600

    Different method for camera following

---
+++ Assets/Scripts/CameraFollowV2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowV2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Transform Target;
+    Rigidbody2D body;
+    void Start()
+    {
+        body = transform.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.MovePosition(Target.transform.position);
+    }
+}
