ea3c405e6d4b5e0475aae5ed614f1a3f6ed57096
Jacob Holst <jakedavidholst@hotmail.com>
2020-04-06 20:23:49 -0500

    Flaire pulls magic when meditating

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -6,16 +6,55 @@ using UnityEngine.Events;
 public class HealthController : MonoBehaviour
 {
     [Range(0, 1000)] [SerializeField] private float health = 100f;
+    [Range(0, 1000)] [SerializeField] private float magic = 0f;
     [Range(0f, 1f)] [SerializeField] private float hurtTimeMultiplier = 1.0f;
 
     private Animator animator;
 
     public bool isDead { get; private set; } = false;
+    public float currentHealth {
+        get
+        {
+            return health;
+        }
+    }
+    public float maxHealth { get; private set; }
+    public bool isAtMaxHealth
+    {
+        get
+        {
+            return currentHealth == maxHealth;
+        }
+    }
+    public float currentMagic
+    {
+        get
+        {
+            return health;
+        }
+    }
+    public float maxMagic { get; private set; }
+    public bool isAtMaxMagic
+    {
+        get
+        {
+            return currentMagic == maxMagic;
+        }
+    }
 
+    public bool isMaxedOut
+    {
+        get
+        {
+            return isAtMaxMagic && isAtMaxHealth;
+        }
+    }
     void Awake()
     {
         animator = GetComponent<Animator>();
         animator?.SetFloat("HurtTimeMultiplier", hurtTimeMultiplier);
+        maxHealth = health;
+        maxMagic = magic;
     }
 
     // Update is called once per frame
@@ -38,8 +77,26 @@ public class HealthController : MonoBehaviour
             animator?.SetTrigger("Hurt");
         }
 
-        health += value;
+        health = ModifyValue(health, maxHealth);
+    }
 
-        if (health < 0) health = 0;
+    public void ModifyMagic(float value)
+    {
+        if (value < 0)
+        {
+            animator?.SetTrigger("Hurt");
+        }
+
+        health = ModifyValue(magic, maxMagic);
+    }
+
+    private float ModifyValue(float value, float maxValue)
+    {
+        value += value;
+
+        if (value < 0) value = 0;
+        if (value > maxValue) value = maxHealth;
+
+        return value;
     }
 }
---
+++ Assets/Scripts/PickupController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            HandlePlayerPickup(collision.gameObject);
+        }
+    }
+
+    private void HandlePlayerPickup(GameObject playerObject)
+    {
+        var healthController = playerObject.GetComponent<HealthController>();
+        if (!healthController.isMaxedOut)
+        {
+            // put sparkles here then set object to inactive
+            // modify health/magic
+        }
+    }
+}
